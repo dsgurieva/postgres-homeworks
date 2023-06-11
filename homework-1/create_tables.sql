bb8b5a4e6aa24bae85d03d0f28116b9c9fac9c1c
dasha <dsgurieva@gmail.com>
2023-06-11 11:53:42 +0500

    commit homework1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employee
+(
+	id_employee int PRIMARY KEY,
+	full_name varchar(100) NOT NULL,
+	job_title varchar(50) NOT NULL,
+	id_customers int REFERENCES customers(id_customers) NOT NULL
+);
+
+INSERT INTO employee VALUES (1, 'Иванов И.И.', 'менеджер', 1);
+
+CREATE TABLE customers
+(
+    id_customers int PRIMARY KEY,
+	full_name varchar(100) NOT NULL,
+	phone_number int NOT NULL,
+	id_orders int REFERENCES orders(id_orders) NOT NULL
+);
+
+INSERT INTO customers VALUES (1, 'Васильев В.В.', 7808, 101);
+
+CREATE TABLE orders
+(
+	id_orders int PRIMARY KEY,
+	name_of_product varchar(100) NOT NULL,
+	price int NOT NULL,
+	quantity int NOT NULL
+);
+
+INSERT INTO orders VALUES (101, 'apple', 1000, 100);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,27 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+with psycopg2.connect(host="localhost", database="north", user="postgres", password="1111") as conn:
+
+    with conn.cursor() as cur:
+
+        # Добавление данных  в таблицу employee
+        cur.execute("INSERT INTO employee VALUES (%s, %s, %s, %s)", (2, 'Петров П.П', 'менеджер', 1))
+        cur.execute("INSERT INTO employee VALUES (%s, %s, %s, %s)", (3, 'Сидоров С.С.', 'менеджер', 1))
+
+        # Добавление данных  в таблицу customers
+        cur.execute("INSERT INTO customers VALUES (%s, %s, %s, %s)", (2, 'Максимов М.М.', 7777, 101))
+        cur.execute("INSERT INTO customers VALUES (%s, %s, %s, %s)", (3, 'Александров А.А.', 7474, 101))
+
+        # Добавление данных  в таблицу orders
+        cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s)", (102, 'tea', 100, 10))
+
+        # Чтение донных из таблицы customers
+        cur.execute("SELECT * FROM customers")
+
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+conn.close()
+
